1d0fb36ddfd2cb3d93f056e79ea9d73194f42454
Muqimjon <muqimjon.mr@gmail.com>
2023-11-13 14:17:56 +0500

    Added documentations

--- src/InfoZest.WebApi/Controllers/InvalidProductsController.cs
+++ src/InfoZest.WebApi/Controllers/InvalidProductsController.cs
@@ -10,22 +10,46 @@ public class InvalidProductsController : BaseController
 {
     public InvalidProductsController(IServices services) : base(services) { }
 
+    /// <summary>
+    /// Add new invalid product
+    /// </summary>
+    /// <param name="dto">The DTO containing additional information for the invalid product</param>
+    /// <returns>Returns the result DTO of the created invalid product</returns>
     [HttpPost("create")]
     public async ValueTask<IActionResult> CreateAsync([FromForm] InvalidProductCreationDto dto)
     => Ok(new Response { Data = await services.InvalidProductService.AddAsync(dto) });
 
+    /// <summary>
+    /// Update excist invalid product
+    /// </summary>
+    /// <param name="dto"></param>
+    /// <returns></returns>
     [HttpPut("update")]
     public async ValueTask<IActionResult> UpdateAsync([FromForm] InvalidProductUpdateDto dto)
     => Ok(new Response { Data = await services.InvalidProductService.ModifyAsync(dto) });
 
+    /// <summary>
+    /// Delete invalid product by Id
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
     [HttpDelete("delete/{id:long}")]
     public async ValueTask<IActionResult> DeleteByIdAsync(long id)
     => Ok(new Response { Data = await services.InvalidProductService.RemoveAsync(id) });
 
+    /// <summary>
+    /// Get invalid product by Id
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
     [HttpGet("get/{id:long}")]
     public async ValueTask<IActionResult> GetByIdAsync(long id)
     => Ok(new Response { Data = await services.InvalidProductService.RetrieveByIdAsync(id) });
 
+    /// <summary>
+    /// Get all invalid products
+    /// </summary>
+    /// <returns></returns>
     [HttpGet("get-all")]
     public async ValueTask<IActionResult> GetAllAsync()
     => Ok(new Response { Data = await services.InvalidProductService.RetrieveAllAsync() });
